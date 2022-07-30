3e09d85434a7138a5a09a1d0f982eb5190775b58
hsinhuachen <yha0971@gmail.com>
2022-07-31 01:01:41 +0800

    fix: 修正回上頁

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,6 @@
 <script>
   if($(".posts.show").length > 0){
-    $( "html:lang(zh-hant)").find(".blog-post").append('<a href="#" class="backBtn">回上頁</a>');
-    $( "html:lang(en)").find(".blog-post").append('<a href="#" class="backBtn">Back</a>');
-    
-    $(".backBtn").on('click', function(event) {
-	    event.preventDefault();
-	    history.go(-1);
-	 });
+    $( "html:lang(zh-hant)").find(".blog-post").append('<a href="https://www.nude4underwear.com/blog/posts" class="backBtn">回上頁</a>');
+    $( "html:lang(en)").find(".blog-post").append('<a href="https://www.nude4underwear.com/blog/posts" class="backBtn">Back</a>');
   }
 </script>
